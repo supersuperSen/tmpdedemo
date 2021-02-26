91d9c16dfea2b0fb709149a87b1114467e84ff07
lisen <1536940785@qq.com>
2021-02-26 11:06:55 +0800

    second

--- src/main/java/com/example/tmpdedemo/Hellolo.java
+++ src/main/java/com/example/tmpdedemo/Hellolo.java
@@ -1,7 +1,7 @@
 package com.example.tmpdedemo;
 
-public class Hello {
+public class Hellolo {
     public static void main(String[] args) {
-        System.err.println("Hello World!!");
+        System.out.println("Hellolo");
     }
 }
